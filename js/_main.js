c9f2cc790d49ea4043ee0ff8917980a93b0cc3e1
iitsuka1513004 <1513004in@gmail.com>
2021-04-10 18:19:05 +0900

    update

--- js/_main.js
+++ js/_main.js
@@ -1,15 +1,61 @@
-$(function() {
-    setTimeout(function() {
-    $(".fog").css({display: 'inline'});
-    $(".fog").addClass("move");
- }, 8500)});
+$(function(){
+  $(".regular").slick({
+    autoplay: true, // 自動再生を設定
+    autoplaySpeed: 4000, // 自動再生のスピード（ミリ秒単位）
+  });
 
- <script>
-function goClick() {
-  // クリック時の動作を指定できます
-  location.href = "https://github.com/iitsuka1513004/info.git"} ;
+  /**
+   * menu
+   */
+   var $nav   = $('#navArea');
+   var $btn   = $('.toggle_btn');
+   var $mask  = $('#mask');
+   var open   = 'open'; // class
+   // menu open close
+   $btn.on( 'click', function() {
+     if ( ! $nav.hasClass( open ) ) {
+       $nav.addClass( open );
+     } else {
+       $nav.removeClass( open );
+     }
+   });
+   // mask close
+   $mask.on('click', function() {
+     $nav.removeClass( open );
+   });
 
-</script>
+});
+ 
+
+//  (function($) {
+//   var $nav   = $('#navArea');
+//   var $btn   = $('.toggle_btn');
+//   var $mask  = $('#mask');
+//   var open   = 'open'; // class
+//   // menu open close
+//   $btn.on( 'click', function() {
+//     if ( ! $nav.hasClass( open ) ) {
+//       $nav.addClass( open );
+//     } else {
+//       $nav.removeClass( open );
+//     }
+//   });
+//   // mask close
+//   $mask.on('click', function() {
+//     $nav.removeClass( open );
+//   });
+// } )(jQuery);
+
+
+//  <script>
+// function goClick() {
+//   // クリック時の動作を指定できます
+//   location.href = ""} ;
+
+// </script>
+
+
+
 //     // 2. スクロールイベント時
 //     $(window).on('scroll', function(){
 //         // 3. スクロール値を変数dyに保存
