da357612685e475acf77b4f35df9f517e45ea0ff
iitsuka1513004 <1513004in@gmail.com>
2021-04-15 16:57:58 +0900

    up

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ $(function(){
     autoplay: true, // 自動再生を設定
     autoplaySpeed: 4000, // 自動再生のスピード（ミリ秒単位）
   });
-  // $('.slider').slick();
+
+  
+  $('.slider').slick();
+
+
 /*** menu
  */
 var $nav   = $('#navArea');
