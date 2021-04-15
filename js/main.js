aabd7dec5ea36770d409f47e834eb76ca1c554fe
iitsuka1513004 <1513004in@gmail.com>
2021-04-15 16:51:38 +0900

    update

--- js/main.js
+++ js/main.js
@@ -2,9 +2,8 @@ $(function(){
   $('.regular').slick({
     autoplay: true, // 自動再生を設定
     autoplaySpeed: 4000, // 自動再生のスピード（ミリ秒単位）
-  
   });
-
+  $('.slider').slick();
 /*** menu
  */
 var $nav   = $('#navArea');
